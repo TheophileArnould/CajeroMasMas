a396e8c087cdddb553cde917751d172ce9a56a81
TheophileArnould <m.theophile.arnould@gmail.com>
2023-09-11 14:28:28 -0500

    prokect creation

---
+++ src/main/java/com/cajeromasmas/CajeroMasMasApplication.java
@@ -0,0 +1,13 @@
+package com.cajeromasmas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CajeroMasMasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CajeroMasMasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cajeromasmas/CajeroMasMasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cajeromasmas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CajeroMasMasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
