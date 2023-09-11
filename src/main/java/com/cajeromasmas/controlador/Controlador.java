4dc1cad0f060fd1b4da76db9153a599e5f776ffa
TheophileArnould <m.theophile.arnould@gmail.com>
2023-09-11 16:47:09 -0500

    creacion del controlador

--- src/main/java/com/cajeromasmas/controlador/Controlador.java
+++ src/main/java/com/cajeromasmas/controlador/Controlador.java
@@ -1,4 +1,34 @@
 package com.cajeromasmas.controlador;
 
+import com.cajeromasmas.modelos.Cuenta;
+import com.cajeromasmas.modelos.Usuario;
+import com.cajeromasmas.servicios.ICuentaServicio;
+import com.cajeromasmas.servicios.ITarjetaServicio;
+import com.cajeromasmas.servicios.IUsuarioServicio;
+import jakarta.validation.constraints.Null;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("")
 public class Controlador {
+    @Autowired
+    private ICuentaServicio cuentaServicio;
+
+    @Autowired
+    private ITarjetaServicio tarjetaServicio;
+
+    @Autowired
+    private IUsuarioServicio usuarioServicio;
+
+    private Usuario usuario = null;
+
+    @GetMapping("/getUsuario/{UsuarioId}")
+    public Usuario getUsuario(@PathVariable Long id){
+        usuario = usuarioServicio.findById(id);
+        return usuario;
+    }
+
+
 }
