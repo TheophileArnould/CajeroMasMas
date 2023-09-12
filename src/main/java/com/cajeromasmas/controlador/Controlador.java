e34f031cced66238054dd49131baee772a1135a1
Santiago Cardona <58612159+SantiCar2@users.noreply.github.com>
2023-09-12 14:32:23 -0500

    Unos nuevos enpoints

--- src/main/java/com/cajeromasmas/controlador/Controlador.java
+++ src/main/java/com/cajeromasmas/controlador/Controlador.java
@@ -8,9 +8,15 @@ import com.cajeromasmas.servicios.IUsuarioServicio;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Null;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.awt.*;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
@@ -26,23 +32,45 @@ public class Controlador {
 
     private Usuario usuario = null;
 
-    @GetMapping("/usuario/{UsuarioId}")
-    public Usuario getUsuario(@PathVariable Long UsuarioId){
-        return usuarioServicio.findById(UsuarioId);
-        //return usuario;
+    @GetMapping("/usuario/{UsuarioId}") //Conectarse a un usuario especifico
+    public Usuario getUsuario(@PathVariable Long UsuarioId) {
+        usuario = usuarioServicio.findById(UsuarioId);
+        return usuario;
     }
 
     @GetMapping("/usuario")
-    public ArrayList<Usuario> getUsuario(){
-        ArrayList<Usuario> usuarios  = (ArrayList<Usuario>) usuarioServicio.findAll();
+    public ArrayList<Usuario> getUsuario() {
+        ArrayList<Usuario> usuarios = (ArrayList<Usuario>) usuarioServicio.findAll();
         return usuarios;
     }
 
     @PostMapping("/usuario")
-    public Usuario creatUsuario(@RequestBody Usuario usuario){
+    public Usuario creatUsuario(@RequestBody Usuario usuario) {
         return usuarioServicio.save(usuario);
     }
 
+    @GetMapping("/cuentas")
+    public ResponseEntity getCuentas() {
+        if (usuario != null) {
+            return new ResponseEntity<ArrayList<Cuenta>>(
+                    (ArrayList<Cuenta>) cuentaServicio.findByUsuarioId(usuario.getId()),
+                    HttpStatus.OK
+            );
+        }
+        return new ResponseEntity<String>("No se conectado a ningun usuario", HttpStatus.UNAUTHORIZED);
+    }
+
+    @PostMapping("/cuenta")
+    public ResponseEntity addCuenta(@RequestBody Cuenta cuenta) {
+        if (usuario != null) {
+            cuenta.setUsuarioId(usuario.getId());
+            return new ResponseEntity<Cuenta>(
+                    cuentaServicio.save(cuenta),
+                    HttpStatus.CREATED
+            );
+        }
+        return new ResponseEntity<String>("No se conectado a ningun usuario", HttpStatus.UNAUTHORIZED);
+    }
 
 
 }
